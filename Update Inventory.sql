30a6fe1e36f6c19cf13bef0983f101ed49de9b6c
Ali Yıldırım <73276002+ali-yildirim@users.noreply.github.com>
2025-01-14 23:00:52 +0300

    Update Update Inventory.sql

--- Update Inventory.sql
+++ Update Inventory.sql
@@ -13,9 +13,9 @@ BEGIN
 
         update master_schedule as ms1, master_schedule ms2
 		set ms1.projected_inventory = case 
-										when  ms2.projected_inventory + ms2.planned_order_receipts - ms2.gross_requirements < 0 then 0
-                                        else  ms2.projected_inventory + ms2.planned_order_receipts - ms2.gross_requirements
-									  end
+					        when  ms2.projected_inventory + ms2.planned_order_receipts - ms2.gross_requirements < 0 then 0
+                                                else  ms2.projected_inventory + ms2.planned_order_receipts - ms2.gross_requirements
+		    			      end
 		where 1=1 
         and ms1.item_id = item_of_interest_id
         and ms1.period_number = period
